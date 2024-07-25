3f167e1e5767c9e11d29bea9b768df7c8644f1d1
Renan Aquino <renan.aquino@grupoboticario.com.br>
2024-07-25 12:10:40 -0300

    test: improvement test

--- __test__/components/Product.test.tsx
+++ __test__/components/Product.test.tsx
@@ -10,5 +10,6 @@ describe('Product', () => {
     test('shoud render product list', () => {
         render(<ProductList />)
         expect(screen.getByText('Produto 1')).toBeInTheDocument();
+        expect(screen.getByText('Produto 2')).toBeInTheDocument();
     })
 });
